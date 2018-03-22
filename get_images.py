d9a5151dba0ade4b3fbb1a3f49780faad6363707
var95 <var6595@gmail.com>
2018-03-22 15:17:45 -0400

    Add files via upload

---
+++ get_images.py
@@ -0,0 +1,80 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import requests
+from subprocess import call
+import os.path
+from PIL import Image
+from StringIO import StringIO
+
+
+# In[2]:
+
+
+# imagenet_urls_df = pd.read_table('fall11_urls.txt', sep="\t", header=None)
+# imagenet_urls_df.columns = ['ID','URL']
+
+
+# # In[12]:
+
+
+# a = imagenet_urls_df.sample(20000)
+
+
+# # In[13]:
+
+
+# a
+
+
+# # In[14]:
+
+
+# a.to_csv(r'imagenet_URLs.txt', header=None, index=None, sep=' ', mode='a')
+
+
+# In[17]:
+
+count=0
+if not os.path.exists('./data'):
+	os.makedirs('./data')
+
+with open("imagenet_URLs.txt","r") as fp:
+	for line in fp:
+		line = line.split(" ")
+		url = line[1]
+		Id = line[0]
+		
+		try:
+			request = requests.get(url)
+		except:
+			print "Broken Link!"
+			continue
+
+		print url, Id
+		if request.status_code == 200:
+			try:
+				img = Image.open(StringIO(request.content)).convert("RGB")
+			except:
+				print "Valid Link, but not valid JPG!"
+				continue
+			count+=1
+			print count
+			print('Web site exists... Downloading Image')
+			call('wget -O data/'+ Id + ".jpg " + url,shell=True)
+			if count == 10000:
+				print("10000 Images Collected!")
+				break
+
+
+
+# img_cover_path = "n12754003_5112.jpg"
+# a =  Image.open(img_cover_path).convert("RGB")
+
+# except:
+				# print "Valid JPG, but not Valid Image"
